97ddf859b545bdc4df860495c476583b9fd3b181
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-02-11 20:38:56 +0530

    setting up cookie with jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const cookie = require("cookie");
 
 // Handling Errors
 const handleErrors = (err) => {
@@ -19,20 +21,32 @@ const handleErrors = (err) => {
   return errors;
 };
 
+const maxAge = 3 * 24 * 60 * 60;
+const createToken = (id) => {
+  return jwt.sign({ id }, "secrete key", {
+    expiresIn: maxAge,
+  });
+};
+
+// Signup page
 module.exports.signup_get = (req, res) => {
   res.render("signup");
 };
 
+// Login page
 module.exports.login_get = (req, res) => {
   res.render("login");
 };
 
+// Signup API
 module.exports.signup_post = async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.create({ email, password });
-    res.status(201).json(user);
+    const token = createToken(user._id);
+    res.cookie("jwt", token, { httpOnly: true, maxAge: maxAge * 1000 });
+    res.status(201).json({ user: user._id });
   } catch (err) {
     console.log(err);
     const errors = handleErrors(err);
@@ -40,6 +54,7 @@ module.exports.signup_post = async (req, res) => {
   }
 };
 
+// Login API
 module.exports.login_post = async (req, res) => {
   const { email } = req.body;
   console.log(email);
