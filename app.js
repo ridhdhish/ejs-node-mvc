0c52544bfa10f14c8d5a3e84b0920953bfbf42d7
ridhdhish <ridhdhishdesai292000@gmail.com>
2021-02-12 11:13:57 +0530

    middleware added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
 const authRoutes = require("./routes/authRoutes");
+
+const { requireAuth } = require("./middleware/authMiddleware");
 
 require("dotenv").config();
 
@@ -9,6 +12,7 @@ const app = express();
 // middleware
 app.use(express.static("public"));
 app.use(express.json());
+app.use(cookieParser());
 
 // view engine
 app.set("view engine", "ejs");
@@ -26,5 +30,5 @@ mongoose
 
 // routes
 app.get("/", (req, res) => res.render("home"));
-app.get("/smoothies", (req, res) => res.render("smoothies"));
+app.get("/smoothies", requireAuth, (req, res) => res.render("smoothies"));
 app.use(authRoutes);
